cb74a9e32693d33970c7e949b6c925b601d39934
trungcheng <trungs1bmt@gmail.com>
2017-12-21 11:21:06 +0700

    Output dynamic data

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 export default class Home extends React.Component {
 	render() {
+		let content = "";
+		if (true) {
+			content = <p>Content generate by variable with condition</p>
+		}
 		return (
 			<div>
 				<p>In a new Component</p>
+				{content}
+				{"Generate by string!"}
 			</div>
 		);
 	}
