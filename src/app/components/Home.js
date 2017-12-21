f969b9b05daabe38cde34d2e761c32dea5377301
trungcheng <trungs1bmt@gmail.com>
2017-12-21 11:44:58 +0700

    props, children and propsType

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,16 +2,30 @@ import React from 'react';
 
 export default class Home extends React.Component {
 	render() {
-		let content = "";
-		if (true) {
-			content = <p>Content generate by variable with condition</p>
-		}
+		var text = "Something!";
 		return (
 			<div>
 				<p>In a new Component</p>
-				{content}
-				{"Generate by string!"}
+				<p>{text}</p>
+				<p>Your name is {this.props.name}</p>
+				<p>Your age is {this.props.age}</p>
+				<p>User Object => Name: {this.props.user.name}</p>
+				<div>
+					<h4>Hobbies</h4>
+					<ul>
+						{this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
+					</ul>
+				</div>
+				<hr />
+				{this.props.children}
 			</div>
 		);
 	}
 }
+
+Home.propTypes = {
+	name: React.PropTypes.string,
+	age: React.PropTypes.number,
+	user: React.PropTypes.object,
+	children: React.PropTypes.element.isRequired
+};
--- src/app/index.js
+++ src/app/index.js
@@ -6,6 +6,10 @@ import Home from './components/Home';
 
 class App extends React.Component {
 	render() {
+		let user = {
+			name: "Test",
+			hobbies: ["Sports", "Reading"]
+		}
 		return (
 			<div className="container">
 				<div className="row">
@@ -15,7 +19,9 @@ class App extends React.Component {
 				</div>
 				<div className="row">
 					<div className="col-xs-10 col-xs-offset-1">
-						<Home />
+						<Home name={"Trung"} age={24} user={user}>
+							<p>This is a paragraph</p>
+						</Home>
 					</div>
 				</div>
 			</div>
